c37c9e368222ff80cc7ae6627dd5773b12c611ac
tomekgadek <gadektomek@gmail.com>
2025-12-31 17:47:23 +0100

    kata-spock, rozbudowano testy dla User'a (tutorial)

--- src/main/kotlin/tutorial/User.kt
+++ src/main/kotlin/tutorial/User.kt
@@ -1,20 +1,28 @@
 package io.github.tomekgadek.tutorial
 
-open class UserInfo(var name: String?, var age: Int)
-
-open class UserEntity(var id: Int, var name: String?, var age: Int)
+enum class UserState {
+    VERIFIED, NEW, UNKNOWN
+}
 
 interface UserRepository {
     fun findUserById(userId: Int): UserEntity?
 }
+
+open class UserInfo(var name: String?, var age: Int, var state: UserState)
+
+open class UserEntity(var id: Int, var name: String?, var age: Int, var state: UserState)
 
 open class UserService(val userRepository: UserRepository) {
 
     fun getUserInfo(userId: Int): UserInfo {
 
         val user = this.userRepository.findUserById(userId)
-        val userInfo = user?.let { UserInfo(it.name, it.age) }
+        val userInfo = user?.let { UserInfo(it.name, it.age, it.state) }
 
-        return userInfo ?: UserInfo("", 0)
+        return userInfo ?: UserInfo("", 0, UserState.UNKNOWN)
+    }
+
+    fun isUserVerified(userId: Int): Boolean {
+        return getUserInfo(userId).state == UserState.VERIFIED
     }
 }
--- src/test/groovy/tutorial/UserSpec.groovy
+++ src/test/groovy/tutorial/UserSpec.groovy
@@ -3,6 +3,7 @@ package tutorial
 import io.github.tomekgadek.tutorial.UserEntity
 import io.github.tomekgadek.tutorial.UserRepository
 import io.github.tomekgadek.tutorial.UserService
+import io.github.tomekgadek.tutorial.UserState
 import spock.lang.Specification
 
 class UserSpec extends Specification {
@@ -13,8 +14,8 @@ class UserSpec extends Specification {
         def userRepository = Stub(UserRepository)
         def userService = Spy(new UserService(userRepository))
 
-        def johnDoe = new UserEntity(1, "John Doe", 25)
-        def mikeJordan = new UserEntity(2, "Mike Jordan", 33)
+        def johnDoe = new UserEntity(1, "John Doe", 25, UserState.NEW)
+        def mikeJordan = new UserEntity(2, "Mike Jordan", 33, UserState.NEW)
 
         userRepository.findUserById(1) >> johnDoe
         userRepository.findUserById(2) >> mikeJordan
@@ -33,8 +34,8 @@ class UserSpec extends Specification {
         def userRepository = Stub(UserRepository)
         def userService = Spy(new UserService(userRepository))
 
-        def johnDoe = new UserEntity(1, "John Doe", 25)
-        def mikeJordan = new UserEntity(2, "Mike Jordan", 33)
+        def johnDoe = new UserEntity(1, "John Doe", 25, UserState.NEW)
+        def mikeJordan = new UserEntity(2, "Mike Jordan", 33, UserState.NEW)
 
         userRepository.findUserById(1) >> johnDoe
         userRepository.findUserById(2) >> mikeJordan
@@ -46,5 +47,25 @@ class UserSpec extends Specification {
         then:
         userInfo.name == ""
         userInfo.age == 0
+        userInfo.state == UserState.UNKNOWN
+    }
+
+    def "shouldReturnTrueWhenUserIsVerified"() {
+
+        given: "prepare data"
+        def userRepository = Stub(UserRepository)
+        def userService = new UserService(userRepository)
+
+        def verifiedUser = new UserEntity(1, "John Doe", 25, UserState.VERIFIED)
+        def unknownUser = null
+
+        userRepository.findUserById(1) >> verifiedUser
+        userRepository.findUserById(2) >> unknownUser
+
+        expect: "user verified => return true"
+        userService.isUserVerified(1)
+
+        and: "unknown user => return false"
+        !userService.isUserVerified(2)
     }
 }
